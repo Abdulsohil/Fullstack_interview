dd4e8c00041f4eaa0a51359a788291980ad83528
AbdulSohil <aslamshaik9573@gmail.com>
2026-02-04 02:26:35 +0530

    cors preflight error rectification1

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const api = axios.create({ baseURL: BASE_URL });
+// const api = axios.create({ baseURL: "http://localhost:5000/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
-
-// axios.post("http://localhost:5000/api/auth/login", data, {
-//   withCredentials: true,
-// });
 
 export default api;
